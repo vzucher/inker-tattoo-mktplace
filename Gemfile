7040b74242af95ac8cbf42a07fd600519a110365
Felipe Americano <felip.americano@gmail.com>
2020-05-09 14:19:20 -0300

    index e show integrados e funcionando

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.5'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 Specialty.create([
   { name: 'Aquarela' },
@@ -31,56 +32,62 @@ Specialty.create([
 )
 
 User.create!(
-    first_name: 'joao',
+    first_name: Faker::Name.name,
     last_name: 'amaral',
     city: 'BH',
     username: 'joaozin',
     experience: 'viajei o mundo todo tatuando e sou fodao',
     email: 'joaozinho@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://tudoparahomens.com.br/wp-content/uploads/2016/07/tatuador-.jpg'
 )
 User.create!(
-    first_name: 'joaofasfa',
+    first_name: Faker::Name.name,
     last_name: 'amarallin',
     city: 'SP',
     username: 'joaozin2',
     experience: 'viajei o mundo todo tatuando e sou fodao2',
     email: 'joaozinho2@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://f.i.uol.com.br/fotografia/2019/03/29/15538955325c9e906cdc68e_1553895532_3x2_md.jpg'
 )
 User.create!(
-    first_name: 'joao3',
+    first_name: Faker::Name.name,
     last_name: 'amaral3',
     city: 'JF',
     username: 'joaozin3',
     experience: 'viajei o mundo todo tatuando e sou fodao3',
     email: 'joaozinho3@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://www.brother.pt/-/media/images/brother-pt/modules/feature-module-images/tatuadores/1170-tatuador.jpg?mw=585&hash=D70A0C9AEA2F020AB762568F4FC5B63AF7FED6C6'
 )
 User.create!(
-    first_name: 'joao4',
+    first_name: Faker::Name.name,
     last_name: 'amaral4',
     city: 'RJ',
     username: 'joaozin4',
     experience: 'viajei o mundo todo tatuando e sou fodao4',
     email: 'joaozinho4@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://saocarlosemrede.com.br/wp-content/uploads/2019/01/21731338_599569217099301_8760591489622192090_n-696x464.jpg'
 )
 User.create!(
-    first_name: 'joao5',
+    first_name: Faker::Name.name,
     last_name: 'amaral5',
     city: 'JUNIO',
     username: 'joaozin5',
     experience: 'viajei o mundo todo tatuando e sou fodao5',
     email: 'joaozinho5@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://wondergirls.com.br/wp-content/uploads/2018/10/Tatuador-Felipe-Eric.jpg'
 )
 User.create!(
-    first_name: 'joao6',
+    first_name: Faker::Name.name,
     last_name: 'amaral6',
     city: 'MG',
     username: 'joaozin6',
     experience: 'viajei o mundo todo tatuando e sou fodao6',
     email: 'joaozinho6@gmail.com',
-    password: 'atalaia123123123'
+    password: 'atalaia123123123',
+    photo: 'https://www.dicasdemulher.com.br/wp-content/uploads/2017/09/como-encontrar-bons-tatuadores-1.jpg'
 )
