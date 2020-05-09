fb693313b32e42e166450f5eaf51572966cb1456
Felipe Americano <felip.americano@gmail.com>
2020-05-09 13:46:34 -0300

    consertei a absorção de dados do DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :tattoo_requests, class_name: "TattooRequest", foreign_key: 'user_id'
   has_many :tattoo_orders, class_name: "TattooRequest", foreign_key: 'tattoo_artist_id'
-  enum kind: [ :user, :tattoo_artist ]
+  enum kind: [ :customer, :tattoo_artist ]
 end
