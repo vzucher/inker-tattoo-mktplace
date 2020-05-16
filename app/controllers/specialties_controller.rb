8042874e49949718e4158b60e0235d0a70cc5f47
Felipe Americano <felip.americano@gmail.com>
2020-05-16 15:26:18 -0300

    rota de specialties liberada e navbar corrigida

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,5 +1,5 @@
 class SpecialtiesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def show
     @specialty = Specialty.find(params[:id])
   end
